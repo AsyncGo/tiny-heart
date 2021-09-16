70b8e1625ad04fc1cc84d8b61de6dbedcf3fcb02
surfin <surfin@surfindeMacBook-Pro.local>
2021-09-16 16:41:32 +0800

    by wangkai

--- birthday-basic/js/login.js
+++ birthday-basic/js/login.js
@@ -1,19 +1,19 @@
 $('#login-button').click(function (event) {
-	var userName=document.getElementById("userName").value;
-    var pwd=document.getElementById("pwd").value;
-		//修改密码请改此处
-    if(userName=="123" &&  pwd=="123"){
+	// var userName=document.getElementById("userName").value;
+ //    var pwd=document.getElementById("pwd").value;
+	// 	//修改密码请改此处
+ //    if(userName=="123" &&  pwd=="123"){
 			event.preventDefault();
-			$('form').fadeOut(500);
+			$('form').fadeOut(100);
 			$('.wrapper').addClass('form-success');
 			requestFullScreen();
 			setTimeout(function(){
 				location.href="BirthdayCake.html";
 			},2000);
-		}
-	else{
-		alert("Wrong Password");
-	}
+		//}
+	// else{
+	// 	alert("Wrong Password");
+	// }
 });
 
 function requestFullScreen(element) {
